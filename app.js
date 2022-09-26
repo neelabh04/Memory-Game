99bddb4b6df70b1c0e845738f8121a39db02b195
Neelabh Singh <neelabh041997@gmail.com>
2022-09-26 11:52:58 +0530

    Shuffling logic and images added

---
+++ app.js
@@ -0,0 +1,59 @@
+const cardArr = [
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    }
+];
+
+/*
+    sort method works by comparing two values. compares the value and sorts through it.
+    math.random returns any number from 0 to -1, we are checking that it's either smaller than 0.5 or larger than it
+    so it will shuffle on the basis of that.
+*/ 
+cardArr.sort(() => 0.5 - Math.random());
+
+console.log(cardArr);
